355d49623fbd382501ca8bf05645d273bf1203a1
theaaliyasheikh <sheikhaaliya57@gmail.com>
2023-10-06 11:09:04 +0530

    Created database for parentForm

--- index.js
+++ index.js
@@ -54,5 +54,15 @@ app.post("/save", (req, res) => {
     connection.query(sql_query, (err, result) => {
         if (err) throw err;
         res.send(result);
-    })
-})
+    });
+});
+
+    app.post("/parentForm", (req, res) => {
+        console.log(req.body);
+        const {droppingGuardian, minorName, fromLocation, toLocation, flightNumber, flightDate, departingGuardianAddress, departingGuardianTelephoneNumber, arrivalGuardianAddress, arrivalGuardianName, arrivalGuardianTelephone} = req.body;
+        const sql_query = `INSERT INTO parent_form (droppingGuardian, minorName, fromLocation, toLocation, flightNumber, flightDate, departingGuardianAddress, departingGuardianTelephoneNumber, arrivalGuardianAddress, arrivalGuardianName, arrivalGuardianTelephone) VALUES ("${droppingGuardian}", "${minorName}", "${fromLocation}", "${toLocation}", "${flightNumber}", "${flightDate}", "${departingGuardianAddress}", "${departingGuardianTelephoneNumber}", "${arrivalGuardianAddress}", "${arrivalGuardianName}", "${arrivalGuardianTelephone}")`;
+        connection.query(sql_query, (err, result) => {
+            if (err) throw err;
+            res.send(result);
+        });
+});
